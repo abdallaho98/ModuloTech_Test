b9e2c38ffbfeecc540b68abbe2347e25c5ba36f6
Abdallah Herzallah <fa_herzallah@esi.dz>
2021-10-02 17:14:10 +0200

    user model

---
+++ app/src/main/java/com/example/modulotech_test/models/Address.kt
@@ -0,0 +1,9 @@
+package com.example.modulotech_test.models
+
+class Address (
+        val city: String,
+        val postalCode: Int,
+        val street: String,
+        val streetCode: String,
+        val country: String
+)
---
+++ app/src/main/java/com/example/modulotech_test/models/User.kt
@@ -0,0 +1,8 @@
+package com.example.modulotech_test.models
+
+class User(
+        val firstName: String,
+        val lastName: String,
+        val address: Address,
+        val birthDate: Long
+)
