ba506e6b865397913c4fc9862ff4ee3961510f11
Abdallah Herzallah <fa_herzallah@esi.dz>
2021-10-06 04:21:16 +0200

    fixes

--- app/src/main/java/com/example/modulotech_test/views/rollershutter/RollerShutterActivity.kt
+++ app/src/main/java/com/example/modulotech_test/views/rollershutter/RollerShutterActivity.kt
@@ -2,11 +2,14 @@ package com.example.modulotech_test.views.rollershutter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.modulotech_test.R
 import com.example.modulotech_test.databinding.ActivityRollerShutterBinding
 import com.example.modulotech_test.helpers.AppPreferencesHelper
+import com.example.modulotech_test.models.Device
 import com.example.modulotech_test.models.RollerShutter
 
 class RollerShutterActivity : AppCompatActivity() {
@@ -20,7 +23,7 @@ class RollerShutterActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         // get Data From Extras
         val prefsHelp = AppPreferencesHelper(this, "data")
-        val device = prefsHelp.getDevices().find {
+        var device = prefsHelp.getDevices().find {
             it.id == intent.getStringExtra("device")
         } as RollerShutter
         title = device?.deviceName
@@ -30,6 +33,17 @@ class RollerShutterActivity : AppCompatActivity() {
         rollerShutterViewModel.setRollerShutter(device)
         binding.viewmodel = rollerShutterViewModel
         binding.lifecycleOwner = this
+        // Observables
+        rollerShutterViewModel.rollerShutter.observe(this , Observer {
+            device = it
+            Log.e("Here details", it.position.toString())
+            prefsHelp.setDevices(
+                prefsHelp.getDevices().map { it2 ->
+                    if(it2.id == device.id) return@map device
+                    return@map it2
+                } as ArrayList<Device>
+            )
+        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
